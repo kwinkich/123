63facf15ae6a3701f8051fdfd0f302f78d93b7a2
kwinkich <vakord21@gmail.com>
2025-01-28 16:32:44 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@app': '/src/app',
-      '@shared': '/src/shared',
-      '@pages': '/src/pages',
-      '@widgets': '/src/widgets',
-      '@store': '/src/store',
-      // Если необходимо, добавьте алиасы для других зависимостей
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@app': '/src/app',
+            '@shared': '/src/shared',
+            '@pages': '/src/pages',
+            '@widgets': '/src/widgets',
+            '@store': '/src/store',
+            // Если необходимо, добавьте алиасы для других зависимостей
+        },
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis', // Заменяем глобальную переменную "global" на "globalThis"
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true, // Добавляем полифилл для process, если он используется
-        }),
-      ],
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: 'globalThis', // Заменяем глобальную переменную "global" на "globalThis"
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                    process: true,
+                }),
+            ],
+        },
     },
-  },
 });
