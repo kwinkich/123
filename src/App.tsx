d0b516bc2e196231452735bdedd5a003a33c86a2
kwinkich <vakord21@gmail.com>
2025-02-01 20:51:10 +0200

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,23 @@
-import { TonConnectButton } from '@tonconnect/ui-react';
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import { useContract } from './useContracts';
-import viteLogo from '/vite.svg';
+import { useState } from "react";
+import "./App.css";
+import { useContract } from "./useContracts";
 
 function App() {
-    const [count, _] = useState(0);
-    const { mint } = useContract();
+	const [count, _] = useState(0);
+	const { mint } = useContract();
 
-    return (
-        <>
-            <div>
-                <TonConnectButton />
-
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => mint()}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
+	return (
+		<>
+			<div className="card">
+				<button
+					style={{ height: "100px", width: "200px", borderRadius: "24px" }}
+					onClick={() => mint()}
+				>
+					НАЕБАЛ
+				</button>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/useContracts.ts
+++ src/useContracts.ts
@@ -1,88 +1,95 @@
-import { OpenedContract } from '@ton/core';
-import { useTonConnectUI } from '@tonconnect/ui-react';
-import { useEffect, useState } from 'react';
-import { Address, Sender, SenderArguments } from 'ton-core';
-import { LingMine } from './wrappers/LingMine';
+import { OpenedContract } from "@ton/core";
+import { useTonConnectUI } from "@tonconnect/ui-react";
+import { useEffect, useState } from "react";
+import { Address, Sender, SenderArguments } from "ton-core";
+import { LingMine } from "./wrappers/LingMine";
 
-import { toNano } from '@ton/core';
-import { useTonClient } from './useTonClient';
+import { toNano } from "@ton/core";
+import { useTonClient } from "./useTonClient";
 
 export function useContract() {
-    const { client } = useTonClient();
-    const [tonConnectUI] = useTonConnectUI();
+	const { client } = useTonClient();
+	const [tonConnectUI] = useTonConnectUI();
 
-    const [contract, setContract] = useState<OpenedContract<LingMine> | null>(null);
-    // const [jettonWallet, setJettonWallet] = useState<OpenedContract<JettonWallet> | null>(null);
+	const [contract, setContract] = useState<OpenedContract<LingMine> | null>(
+		null
+	);
+	// const [jettonWallet, setJettonWallet] = useState<OpenedContract<JettonWallet> | null>(null);
 
-    const [sender, setSender] = useState<Sender>();
+	const [sender, setSender] = useState<Sender>();
 
-    useEffect(() => {
-        if (!client) return;
+	useEffect(() => {
+		if (!client) return;
 
-        const loadContract = async () => {
-            try {
-                const contr = LingMine.createFromAddress(
-                    Address.parse('kQBmjPPYaw6Zu8rS-cSYBVd5K6Vbq4raTyqwc0EMrAL4fIW3'),
-                );
-                // @ts-ignore
-                const openedContract = client.open(contr) as unknown as OpenedContract<LingMine>;
-                console.log('opened contr', openedContract);
+		const loadContract = async () => {
+			try {
+				const contr = LingMine.createFromAddress(
+					Address.parse("kQBLnE_2HHOBSHUpcWWWzyBC7SAjGVhHrVUXEpnWx9JWPlXL")
+				);
+				// @ts-ignore
+				const openedContract = client.open(
+					contr
+				) as unknown as OpenedContract<LingMine>;
+				console.log("opened contr", openedContract);
 
-                setContract(openedContract);
-            } catch (error) {
-                console.error('Failed to load contract:', error);
-            }
-        };
+				setContract(openedContract);
+			} catch (error) {
+				console.error("Failed to load contract:", error);
+			}
+		};
 
-        loadContract();
-    }, [client]);
+		loadContract();
+	}, [client]);
 
-    // useEffect(() => {
-    //     if (!contract || !tonConnectUI.account?.address) return;
+	// useEffect(() => {
+	//     if (!contract || !tonConnectUI.account?.address) return;
 
-    //     const loadJettonWallet = async () => {
-    //         try {
-    // 						// @ts-ignore
-    //             const jtAddr = await contract.getWalletAddress(
-    //                 Address.parse(tonConnectUI.account?.address || "")
-    //             );
-    //             const openedJettonWallet = client?.open(
-    //                 JettonWallet.createFromAddress(jtAddr)
-    //             ) as OpenedContract<JettonWallet>;
-    //             setJettonWallet(openedJettonWallet);
-    //         } catch (error) {
-    //             console.error("Failed to load Jetton Wallet:", error);
-    //         }
-    //     };
+	//     const loadJettonWallet = async () => {
+	//         try {
+	// 						// @ts-ignore
+	//             const jtAddr = await contract.getWalletAddress(
+	//                 Address.parse(tonConnectUI.account?.address || "")
+	//             );
+	//             const openedJettonWallet = client?.open(
+	//                 JettonWallet.createFromAddress(jtAddr)
+	//             ) as OpenedContract<JettonWallet>;
+	//             setJettonWallet(openedJettonWallet);
+	//         } catch (error) {
+	//             console.error("Failed to load Jetton Wallet:", error);
+	//         }
+	//     };
 
-    //     loadJettonWallet();
-    // }, [contract, tonConnectUI.account?.address]);
+	//     loadJettonWallet();
+	// }, [contract, tonConnectUI.account?.address]);
 
-    useEffect(() => {
-        let sender: Sender = {
-            address: Address.parse(tonConnectUI.account?.address || 'kQBmjPPYaw6Zu8rS-cSYBVd5K6Vbq4raTyqwc0EMrAL4fIW3'), // Здесь исправлено на Address.parse
-            send: async (args: SenderArguments) => {
-                tonConnectUI.sendTransaction({
-                    messages: [
-                        {
-                            address: args.to.toString(),
-                            amount: args.value.toString(),
-                            payload: args.body?.toBoc().toString('base64'),
-                        },
-                    ],
-                    validUntil: Date.now() + 5 * 60 * 1000,
-                });
-            },
-        };
+	useEffect(() => {
+		let sender: Sender = {
+			address: Address.parse(
+				tonConnectUI.account?.address ||
+					"kQBLnE_2HHOBSHUpcWWWzyBC7SAjGVhHrVUXEpnWx9JWPlXL"
+			), // Здесь исправлено на Address.parse
+			send: async (args: SenderArguments) => {
+				tonConnectUI.sendTransaction({
+					messages: [
+						{
+							address: args.to.toString(),
+							amount: args.value.toString(),
+							payload: args.body?.toBoc().toString("base64"),
+						},
+					],
+					validUntil: Date.now() + 5 * 60 * 1000,
+				});
+			},
+		};
 
-        setSender(sender);
-    }, [tonConnectUI]);
+		setSender(sender);
+	}, [tonConnectUI]);
 
-    const to = Address.parse('0QD1JmW0OlSk5Nmz9ZhzF0sgQU4C9LRt8GjwjgoVEbwXkJWy'); // Исправлено на Address.parse
+	const to = Address.parse("0QD1JmW0OlSk5Nmz9ZhzF0sgQU4C9LRt8GjwjgoVEbwXkJWy"); // Исправлено на Address.parse
 
-    return {
-        // jettonWalletAddress: jettonWallet?.address.toString(),
-        // @ts-ignore
-        mint: () => contract?.sendTx(sender, toNano('0.8'), 1234n),
-    };
+	return {
+		// jettonWalletAddress: jettonWallet?.address.toString(),
+		// @ts-ignore
+		mint: () => contract?.sendTx(sender, toNano("0.55"), 1234n),
+	};
 }
